3424bf63781fd022efcd577a10a63e2370c084d0
Максии <61458872+Max0n22@users.noreply.github.com>
2024-06-13 14:28:38 +0300

    Update adaptive.js

--- js/adaptive.js
+++ js/adaptive.js
@@ -1,10 +1,11 @@
-
 function matchImageWidths() {
     const firstImage = document.getElementById('firstImage');
     const secondImage = document.getElementById('secondImage');
+    const thirdImage = document.getElementById('thirdImage');
 
     const firstImageWidth = firstImage.offsetWidth;
     secondImage.style.width = firstImageWidth + 'px';
+    thirdImage.style.width = firstImageWidth + 'px';
 }
 
 window.addEventListener('load', matchImageWidths);
